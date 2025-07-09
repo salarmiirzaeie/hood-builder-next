e5697c96e72876069e9337a0ac5258a4813e6639
Salar Miirzapour <salar.miirzapour@wonderdays.com>
2025-07-09 09:35:16 +0330

    rm unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Clients from "./components/home/Clients";
 import Description from "./components/home/Discription";
 import ServicesBanner from "./components/home/services-banner";
 import BlogPostsSection from "./components/home/Blogs";
-import ImageSlider from "./components/home/ImageSlider";
+// import ImageSlider from "./components/home/ImageSlider";
 import FireSuppressionSection from "./components/home/FireSuppressionSection";
 import RestaurantInfoSection from "./components/home/RestaurantInfoSection";
 import AboutBanner from "./components/home/about-banner";
