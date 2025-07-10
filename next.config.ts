588b2f93166415484456c6b970fa336213c22fb7
Salar Miirzapour <salar.miirzapour@wonderdays.com>
2025-07-10 11:12:17 +0330

    fix config to static

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // for static export
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   // reactStrictMode: true,
   // experimental: {
   //   optimizeCss: true,
